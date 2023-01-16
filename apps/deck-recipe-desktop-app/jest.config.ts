2f6540bab3b02385762e7c0aeaa90a9884da4e77
shuufei <digital.shuufei@gmail.com>
2023-01-16 18:50:18 +0900

    Initial commit

---
+++ apps/deck-recipe-desktop-app/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'deck-recipe-desktop-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/deck-recipe-desktop-app',
+};
