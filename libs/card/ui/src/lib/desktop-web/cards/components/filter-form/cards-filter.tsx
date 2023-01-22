d6c5e47c8dcaf91c50b58fba4399747e585742c5
shuufei <digital.shuufei@gmail.com>
2023-01-22 21:58:05 +0900

    feat: add filter reset button

--- libs/card/ui/src/lib/desktop-web/cards/components/filter-form/cards-filter.tsx
+++ libs/card/ui/src/lib/desktop-web/cards/components/filter-form/cards-filter.tsx
@@ -1,5 +1,12 @@
 import { Button, HStack, VStack } from '@chakra-ui/react';
-import { FC, memo } from 'react';
+import { FC, memo, useCallback } from 'react';
+import { useRecoilState } from 'recoil';
+import {
+  CardTypeCondition,
+  ColorsCondition,
+  filterConditionState,
+  LvCondition,
+} from '../../state/filter-conditions';
 import { CardTypeFilter } from './card-type-filter';
 import { CategoryFilter } from './category-filter';
 import { ColorsFilter } from './colors-filter';
@@ -9,6 +16,26 @@ import { LvFilter } from './lv-filter';
 export const CardsFilter: FC<{
   onExecuteFilter: () => void;
 }> = memo(({ onExecuteFilter }) => {
+  const [, setFilterCondition] = useRecoilState(filterConditionState);
+  const resetFilter = useCallback(() => {
+    setFilterCondition((current) => {
+      const resetCondition = (condition: { [key: string]: boolean }) =>
+        Object.keys(condition).reduce((acc, curr) => {
+          return {
+            ...acc,
+            [curr]: true,
+          };
+        }, {});
+      return {
+        category: resetCondition(current.category),
+        color: resetCondition(current.color) as ColorsCondition,
+        lv: resetCondition(current.lv) as LvCondition,
+        cardType: resetCondition(current.cardType) as CardTypeCondition,
+        includeParallel: true,
+      };
+    });
+    onExecuteFilter();
+  }, [onExecuteFilter, setFilterCondition]);
   return (
     <VStack alignItems={'flex-start'}>
       <HStack alignItems={'center'}>
@@ -18,9 +45,19 @@ export const CardsFilter: FC<{
         <CardTypeFilter />
         <IncludeParallelFilter />
       </HStack>
-      <Button size={'sm'} colorScheme={'blue'} onClick={onExecuteFilter}>
-        再検索
-      </Button>
+      <HStack>
+        <Button size={'sm'} colorScheme={'blue'} onClick={onExecuteFilter}>
+          再検索
+        </Button>
+        <Button
+          size={'sm'}
+          variant={'outline'}
+          colorScheme={'blue'}
+          onClick={resetFilter}
+        >
+          リセット
+        </Button>
+      </HStack>
     </VStack>
   );
 });
