c64843b68b15c0006ce61c7ec8905320f841d680
shuufei <digital.shuufei@gmail.com>
2023-02-02 23:59:27 +0900

    feat: デジタマエリアのaction実装

--- libs/vs/ui/src/lib/pages/board/components/board/digitama-stack-area.tsx
+++ libs/vs/ui/src/lib/pages/board/components/board/digitama-stack-area.tsx
@@ -1,12 +1,15 @@
 import { Text, VStack } from '@chakra-ui/react';
-import { FC, memo, useContext } from 'react';
+import { FC, memo, useCallback, useContext } from 'react';
 import { useRecoilValue } from 'recoil';
 import { CARD_WIDTH } from '../../constants/card-width';
 import { PlayerContext } from '../../context/player-context';
+import { useShuffleBoardArea } from '../../hooks/use-shuffle-area';
 import { useDispatcher } from '../../state/dispatcher';
 import { boardDigitamaStackAreaSelector } from '../../state/selectors/board-digitama-stack-area-selector';
 import { AreaEmptyImg } from '../area-empty-img';
 import { DigitamaCardBackImg } from '../digitama-card-back-img';
+import { ActionMenu, ActionMenuItem } from './actioin-menu';
+import { AREA_ACTIONS } from './board-actions';
 
 export const DigitamaStackArea: FC = memo(() => {
   const player = useContext(PlayerContext);
@@ -14,21 +17,55 @@ export const DigitamaStackArea: FC = memo(() => {
     boardDigitamaStackAreaSelector(player)
   );
   const dispatch = useDispatcher();
+  const shuffleArea = useShuffleBoardArea();
 
-  const draw = () => {
-    if (player === 'opponent') {
-      return;
-    }
-    dispatch('me', {
-      actionName: 'draw',
-      data: undefined,
-    });
-  };
+  const actionMenuItems: ActionMenuItem[] = [
+    AREA_ACTIONS.hatching,
+    AREA_ACTIONS.shuffle,
+  ];
+
+  const onClickActionMenuItem = useCallback(
+    (actionId: ActionMenuItem['id']) => {
+      switch (actionId) {
+        case 'hatching': {
+          if (digitamaStackArea.length <= 0) {
+            return;
+          }
+          const card = digitamaStackArea[0];
+          dispatch('me', {
+            actionName: 'move',
+            data: {
+              srcArea: 'digitamaStack',
+              destArea: 'standby',
+              card: card,
+              position: 'bottom',
+            },
+          });
+          return;
+        }
+        case 'shuffle':
+          shuffleArea('stack');
+          return;
+      }
+    },
+    [digitamaStackArea, dispatch, shuffleArea]
+  );
 
   return (
     <VStack spacing={1}>
       {digitamaStackArea.length > 0 ? (
-        <DigitamaCardBackImg width={CARD_WIDTH} onClick={draw} />
+        player === 'me' ? (
+          <ActionMenu
+            actionMenuItems={actionMenuItems}
+            onClickAction={(id) => {
+              onClickActionMenuItem(id);
+            }}
+          >
+            <DigitamaCardBackImg width={CARD_WIDTH} />
+          </ActionMenu>
+        ) : (
+          <DigitamaCardBackImg width={CARD_WIDTH} />
+        )
       ) : (
         <AreaEmptyImg width={CARD_WIDTH} />
       )}
