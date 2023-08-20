de15440f4f0afc7aa34a874f03865762ad2a8f4b
shuufei <digital.shuufei@gmail.com>
2023-08-20 17:20:20 +0900

    Update set-cookie-app-access-key.ts

--- apps/desktop-app/pages/api/set-cookie-app-access-key.ts
+++ apps/desktop-app/pages/api/set-cookie-app-access-key.ts
@@ -6,6 +6,7 @@ export const APP_ACCESS_CHECK_KEY = 'beelzebub-access-key';
 const handler: NextApiHandler = (req, res) => {
   const query = req.query;
   const correctRequest = query.key === process.env.NEXT_BEELZEBUB_ACCESS_KEY;
+  console.log('----', correctRequest, query, process.env.NEXT_BEELZEBUB_ACCESS_KEY)
   if (req.method !== 'GET' || !correctRequest) {
     return res.status(500).json({ message: 'can not access!' });
   }
